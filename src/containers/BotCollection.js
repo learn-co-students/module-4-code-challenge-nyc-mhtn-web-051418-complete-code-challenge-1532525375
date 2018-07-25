c53309eab570abc4417495b81f3271254b1a2691
github username <github email address>
2018-07-25 10:59:22 -0400

    Done.

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -8,12 +8,7 @@ class BotCollection extends React.Component {
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-
           {this.props.botList.map(bot => <BotCard key={bot.id} bot={bot} chosenBot={this.props.chosenBot} />)}
-
-    		  Collection of all bots
-
-          HELLO WORLD! THIS IS A TEST
     		</div>
   	  </div>
   	);
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -4,7 +4,9 @@ import YourBotArmy from "./YourBotArmy"
 
 const url = "https://bot-battler-api.herokuapp.com/api/v1/bots"
 
-  let userSelection = []; //Not ideal. I should not be doing this with State.
+  let userSelection = []; //Not ideal. I should not be doing this with State. //Put up here because each click would reset the array if the array was declared/assigned as empty in the chosenBot method.
+
+  //Just wanted to get something to display.
 
 class BotsPage extends React.Component {
   //start here with your code for step one
@@ -21,14 +23,14 @@ class BotsPage extends React.Component {
 
 
   chosenBot = (event) => {
-    //Note: On this method I was having difficult grabbing the entire bot, rather than just one element. I was adding my event listener too far down tree and ran out of time.
+    //Note: On this method I was having difficult grabbing the entire bot, rather than just one DOM element. I think I was adding my event listener too far down tree and ran out of time.
 
-    event.persist();
+    event.persist();//only used for testing
     console.log("You clicked a bot!!!")
 
     userSelection.push(event.target)
-    //
-    this.setState( {userSelection: userSelection} )
+
+    this.setState( {userSelection: userSelection} ) //updating state
   }
 
   componentDidMount(){
@@ -41,13 +43,12 @@ class BotsPage extends React.Component {
     console.log("BotsPage with fetch", this.state.userSelection)
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy userSelection={this.state.userSelection}/>
         <BotCollection
           botList={this.state.bots}
           chosenBot={this.chosenBot}
           userSelection={this.state.userSelection}
           />
-        <YourBotArmy userSelection={this.state.userSelection}/>
       </div>
     );
   }
