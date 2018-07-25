5e87ce561eb3f52df92e1081304ebdf9c3ed606a
JohnZBurnett <tuninator@gmail.com>
2018-07-25 09:44:20 -0400

    First commit

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -2,6 +2,11 @@ import React from "react";
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+  constructor() {
+    this.state = {
+      
+    }
+  }
 
   render() {
     return (
