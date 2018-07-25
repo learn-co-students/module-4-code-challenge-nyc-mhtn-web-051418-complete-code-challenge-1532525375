2152920fa45d719ff1872756d7d651c3146440b6
JohnZBurnett <tuninator@gmail.com>
2018-07-25 10:51:38 -0400

    implement filter and sort

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -7,12 +7,12 @@ class BotCollection extends React.Component {
 		super(); 
 
 		this.state = {
-			classToFilterBy: ""
+			classToFilterBy: "",
+			statToSortBy: "health"
 		}
 	}
 
 	filterBots = () => {
-		const currentBotList = this.props.bots.slice(); 
 		const filteredBotList = this.props.bots.filter(bot => {
 			return(
 				bot.bot_class.includes(this.state.classToFilterBy)
@@ -22,28 +22,51 @@ class BotCollection extends React.Component {
 	}
 	
 	handleBotClassChange = (event) => {
-		console.log("Dropdown Value: ", event.target.value);
 		this.setState({
 			classToFilterBy: event.target.value
 		})
 	}
 
+	handleSortStatChange = (event) => {
+		this.setState({
+			statToSortBy: event.target.value,
+		}); 
+	}
+
+	handleFilteringAndSortingBots = () => {
+		const filteredBotList = this.filterBots(this.props.bots);
+		return this.sortBots(filteredBotList);
+	}
+
 	renderAllBotsAsCards = () => {
-		return this.filterBots().map( (bot) => {
+		return this.handleFilteringAndSortingBots().map( (bot) => {
 			return <BotCard bot={bot} key={bot.id} id={bot.id} onClick={this.props.onBotClick}/>
 		})
 	}
 
+	sortBots = (botList) => {
+		return botList.sort( (botA, botB) => {
+			return botB[this.state.statToSortBy] - botA[this.state.statToSortBy]
+		})
+	}
+	
+
   render(){
-		this.filterBots(); 
+		this.handleFilteringAndSortingBots(); 
   	return (
   	  <div className="ui four column grid">
 				<label>Filter bots based on class: </label>
-			  <select defaultValue="" value={this.state.classToFilterBy} onChange={this.handleBotClassChange}>
+			  <select value={this.state.classToFilterBy} onChange={this.handleBotClassChange}>
 			   	<option value="">All</option>
 					<option value="Support">Support</option>
 					<option value="Assault">Assault</option>
 					<option value="Defender">Defender</option>
+				</select>
+				<label>Sort bots based on this stat: </label>
+				<select value={this.state.statToSortBy} onChange={this.handleSortStatChange}>
+				  <option value="health">Health</option>
+					<option value="damage">Damage</option>
+					<option value="armor">Armor</option>
 				</select>
     		<div className="row">
 				  {this.renderAllBotsAsCards()}
